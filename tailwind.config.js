d4649593b9d5d52b87fa65616df0e3447b88feb6
arionkosturi <arionkosturi@gmail.com>
2024-05-18 23:24:37 +0200

    changed background color

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    colors: {
+      'brand_dark': '#0D1116',
+    },
+    extend: {},
+  },
+  // darkMode: 'selector',
+  plugins: [],
+}
+
