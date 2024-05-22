702eb31f092ee7c39c0e9b900a777034f012629f
arkotech <arionkosturi@gmail.com>
2024-05-22 16:01:35 +0200

    test

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
-i = 1
+// Të shkruhet programi që simulon funksionalitetin e metodës map()
+//function _map(arr, f) { 
+    // kodi juaj 
+// }
 
-do {
-    console.log(i)
-    i++
-}while(i <= 10);
+
+const arr = [4, 5, 10, 3, 8, 6];
+let result = [];
+
+const dyfisho = function() {
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i] * 2);
+    }
+    return result
+}
+
+console.log(dyfisho(arr));
